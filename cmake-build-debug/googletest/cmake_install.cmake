142a5fe10aa6c1f882abafcbd89f9079338c0bc2
Arsenii Fadieiev <fadiears@fel.cvut.cz>
2025-01-10 17:26:45 +0100

    GoogleTest added

--- cmake-build-debug/googletest/cmake_install.cmake
+++ cmake-build-debug/googletest/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/senec/CLionProjects/Sudoku
+# Install script for directory: C:/Users/senec/CLionProjects/Sudoku/googletest
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -39,17 +39,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/Users/senec/CLionProjects/Sudoku/cmake-build-debug/googletest/cmake_install.cmake")
+  include("C:/Users/senec/CLionProjects/Sudoku/cmake-build-debug/googletest/googlemock/cmake_install.cmake")
 
 endif()
 
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/senec/CLionProjects/Sudoku/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
